5551b9b46273024415dab9c0695e6839b43eb01d
drxgons <omarlemuz@outlook.com>
2019-08-20 23:55:19 -0500

    Primer commit

---
+++ Calculadora/Calculadora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Omar Lemuz on 8/20/19.
+//  Copyright © 2019 Omar Lemuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculadora/CalculadoraTests/CalculadoraTests.swift
@@ -0,0 +1,34 @@
+//
+//  CalculadoraTests.swift
+//  CalculadoraTests
+//
+//  Created by Omar Lemuz on 8/20/19.
+//  Copyright © 2019 Omar Lemuz. All rights reserved.
+//
+
+import XCTest
+@testable import Calculadora
+
+class CalculadoraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
